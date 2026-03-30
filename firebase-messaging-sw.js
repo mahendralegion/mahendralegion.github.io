6a757fbc8e4e9aa04b6ea51efef302e5bcca47f5
mahendralegion <mahendra@legion.is>
2026-03-30 12:50:47 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,5 +3,5 @@
 // importScripts("https://cdn.subscribersdemo.com/assets/subscribers-sw.js");
 
 //prod
-var version = "1.5.1";
-importScripts("https://cdn.subscribers.com/assets/subscribers-sw.js");
+// var version = "1.5.1";
+// importScripts("https://cdn.subscribers.com/assets/subscribers-sw.js");
